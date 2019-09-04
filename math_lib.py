728defbec579c80d77d00804fb29ecd10d749fc6
Theodore Fobe <theodore.fobe@colorado.edu>
2019-09-04 12:00:12 -0600

    added library and function docstrings and accounted for divide by 0 case

--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,22 @@
+"""
+This module contains basic math operations
+"""
+
 def div(a, b):
-    return a/b
+    """
+    Takes the quotient of two numbers
+
+    Argumnets
+    ---------
+    a : float
+    b : float
+
+    Returns
+    -------
+    q : float
+        quotient of a and b.
+    """
+    if b == 0:
+        return None
+    else:
+        return a/b
